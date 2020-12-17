42c35882d01421ed4ceafe73ac327379ee0cd86c
Christian Quintero <chriswbe1993@gmail.com>
2020-12-17 14:21:27 -0500

    Se añade boton de favoritos, de deshabilita el mismo si el post ya está dentro de la lista de favoritos

--- app/src/main/java/com/example/pruebatecnica/DialogActivity.java
+++ app/src/main/java/com/example/pruebatecnica/DialogActivity.java
@@ -1,38 +1,85 @@
 package com.example.pruebatecnica;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.pruebatecnica.models.home.HomeModel;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DialogActivity extends AppCompatActivity {
     HomeModel item;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
+    TextView userId;
+    TextView id;
+    TextView name;
+    TextView username;
+    TextView email ;
+    TextView phone;
+    TextView website;
+    Button btnFav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
-
+        sharedPreferences = this.getSharedPreferences(getString(R.string.preference_file_fav), Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
         Bundle parametros = this.getIntent().getExtras();
 
         try {
             item = new HomeModel(new JSONObject(parametros.getString("item")));
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        TextView userId = findViewById(R.id.tvUserIdDialog);
-        TextView id = findViewById(R.id.tvIdDialog);
-        TextView name = findViewById(R.id.tvNameDialog);
-        TextView username = findViewById(R.id.tvUseNameDialog);
-        TextView email = findViewById(R.id.tvEmailDialog);
-        TextView phone = findViewById(R.id.tvPhoneDialog);
-        TextView website = findViewById(R.id.tvWebsiteDialog);
+        initViews();
 
+        btnFav.setEnabled(obtenerFavShared());
+
+        btnFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String itemId = String.valueOf(item.getId());
+                editor.putString(itemId, itemId);
+                editor.apply();
+                btnFav.setEnabled(false);
+            }
+        });
+
+
+    }
+
+    public boolean obtenerFavShared(){
+        String listString = sharedPreferences.getString(String.valueOf(item.getId()),"null");
+        Log.d("MVVM", listString);
+        return listString=="null";
+    }
+
+    public void initViews(){
+
+        userId = findViewById(R.id.tvUserIdDialog);
+        id = findViewById(R.id.tvIdDialog);
+        name = findViewById(R.id.tvNameDialog);
+        username = findViewById(R.id.tvUseNameDialog);
+        email = findViewById(R.id.tvEmailDialog);
+        phone = findViewById(R.id.tvPhoneDialog);
+        website = findViewById(R.id.tvWebsiteDialog);
+        btnFav = findViewById(R.id.btnFavoritos);
 
         userId.setText(String.valueOf(item.getUserId()));
         id.setText(String.valueOf(item.getId()));
--- app/src/main/java/com/example/pruebatecnica/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/pruebatecnica/ui/home/HomeFragment.java
@@ -88,7 +88,7 @@ public class HomeFragment extends Fragment {
         rvLista.setLayoutManager(manager);
 
         items = new ArrayList<>();
-        items.add(new HomeModel(1, 1, "title", "body", "Christian","chriswbe","chriswbe1993@gmail.com","123456789","google.com",R.drawable.user));
+        items.add(new HomeModel(1, 1, "title", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "Christian","chriswbe","chriswbe1993@gmail.com","123456789","google.com",R.drawable.user));
         items.add(new HomeModel(1, 2, "title", "body", "Christian","chriswbe","chriswbe1993@gmail.com","123456789","google.com",R.drawable.user));
         items.add(new HomeModel(1, 3, "title", "body", "Christian","chriswbe","chriswbe1993@gmail.com","123456789","google.com",R.drawable.user));
         items.add(new HomeModel(1, 4, "title", "body", "Christian","chriswbe","chriswbe1993@gmail.com","123456789","google.com",R.drawable.user));
